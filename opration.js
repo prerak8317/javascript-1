a324baca809c7bfd3de521e65263ab578396f5dd
prerak <prerak511@gmail.com>
2025-02-27 16:59:06 +0530

    oprations

---
+++ opration.js
@@ -0,0 +1,41 @@
+// arthmatic calculation
+// let a = 25;
+// let b = 4;
+// console.log("a + b =", a + b);
+// console.log("a - b =", a - b);
+// console.log("a * b =", a * b);
+// console.log("a / b =", a / b);
+// console.log("a % b =", a % b);
+// console.log("a ** b =", a ** b);
+// console.log("a + b =", a++);
+// console.log("a + b =", a--);
+// console.log("a + b =", ++a);
+// console.log("a + b =", --a);
+
+//assinment opration
+// let p = 10;
+// p += 5;
+// p -= 5;
+// p *= 5;
+// p /= 5;
+// p %= 5;
+// p **= 5;
+// console.log(p);
+
+//comparision opration
+// let p = 10;
+// let n = 20;
+// console.log("p == n is", p == n);
+// console.log("p != n is", p != n);
+// console.log("p === n is", p === n);
+// console.log("p != n is", p !== n);
+// console.log("p > n is", p > n);
+// console.log("p < n is", p < n);
+// console.log("p < n is", p >= n);
+// console.log("p < n is", p <= n);
+
+//logical opration
+let p = 4;
+let n = 5;
+console.log(p < n && p == n);
+console.log(p < n || p == n);
